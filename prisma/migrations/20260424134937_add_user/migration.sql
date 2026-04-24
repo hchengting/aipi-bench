f5b25032ddcf8bb9fcd80bf6456f1d29bce2f744
jaroslawjanas <jaroslawjanas.dev@gmail.com>
2026-04-24 21:52:02 +0200

    Add missing User table migration

---
+++ prisma/migrations/20260424134937_add_user/migration.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'user',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
